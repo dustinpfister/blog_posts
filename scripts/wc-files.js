f64a0405f24832f0c4bd46edc2745f7af690db02
dustinpfister <dustin.pfister@gmail.com>
2019-03-31 13:37:45 -0400

    wc-files custom function working

--- scripts/wc-files.js
+++ scripts/wc-files.js
@@ -1,3 +1,19 @@
-let klawFiles = require('./klaw.js').klawFiles;
+let klawFiles = require('./klaw.js').klawFiles,
+fs = require('fs')
 
-klawFiles();
+klawFiles(function(item){
+	
+	//console.log(item.path);
+	
+	fs.readFile(item.path, function(e,data){
+		
+		if(data){
+			
+			console.log(data.toString());
+			
+		}
+		
+		
+	})
+	
+});
