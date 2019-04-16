9a06fefd80a243ba80f2f3b27f41bd9c63d003c4
dustinpfister <dustin.pfister@gmail.com>
2019-04-16 14:26:32 -0400

    can set date range from cli

--- scripts/cli/to-update/index.js
+++ scripts/cli/to-update/index.js
@@ -1,24 +1,31 @@
 let klawFiles = require('../../klaw.js').klawFiles;
 
-let yearHigh = 2017,
-yearLow = 0,
-monthHigh = 12,
-monthLow = 01;
+let argv = process.argv,
 
+yearHigh = argv[2] || new Date().getFullYear() - 1,
+monthHigh = argv[3] || 12,
+
+yearLow = argv[4] || 1970,
+monthLow = argv[5] || 1;
+
+let dateHigh = new Date(yearHigh, monthHigh - 1, new Date(yearHigh, monthHigh, 0).getDate()),
+dateLow = new Date(yearLow, monthLow - 1, 1);
+
+// klaw files
 klawFiles({
 
-    dir_posts: '../../../_posts',
+    //dir_posts: '../../../_posts',
     forFile: (item, next) => {
 
-        let lastUpdate = new Date(item.header.updated),
-        luY = lastUpdate.getFullYear(),
-        luM = lastUpdate.getMonth() + 1;
+        let lu = new Date(item.header.updated),
+        luY = lu.getFullYear(),
+        luM = lu.getMonth() + 1;
 
         // if the date at which the post was last updated falls
         // between the set range.
-        if (luY <= yearHigh && luY >= yearLow && luM <= monthHigh && luM >= monthLow) {
+        if (lu.getTime() <= dateHigh.getTime() && lu.getTime() >= dateLow.getTime()) {
             // then log
-            console.log(item.fn, '(' + luY + '/' + luM + ')');
+            console.log(item.fn, '(' + luY + '/' + luM + '/' + lu.getDate() + ')');
         }
 
         next();
