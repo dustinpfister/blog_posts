a0a8ca428833c498bf35282988bc3163b48d8d7b
dustinpfister <dustin.pfister@gmail.com>
2019-04-16 21:14:37 -0400

    next-id now uses klaw basic

--- scripts/cli/next-id/index.js
+++ scripts/cli/next-id/index.js
@@ -1,14 +1,29 @@
+/*
 let klawFiles = require('../../klaw.js').klawFiles;
 
 let ct = 0;
 klawFiles({
-    read: false,
-    forFile: (item, next) => {
+read: false,
+forFile: (item, next) => {
+ct += 1;
+next();
+},
+onDone: () => {
+console.log(ct); // should be the id
+}
+
+});
+ */
+
+let klawPosts = require('../klaw-basic').klawPosts,
+ct = 0;
+
+klawPosts({
+    forPost: (item, next) => {
         ct += 1;
         next();
     },
     onDone: () => {
-        console.log(ct); // should be the id
+        console.log(ct);
     }
-
 });
