763dc41bf869e273d3fa6f2cf4419d88fa84d048
dustinpfister <dustin.pfister@gmail.com>
2019-04-16 20:43:53 -0400

    clean up

--- scripts/cli/wc-cat-days/index.js
+++ scripts/cli/wc-cat-days/index.js
@@ -9,16 +9,12 @@ let wcCatDays = function (opt) {
 
     opt = Object.assign({}, opt_defaults, opt || {});
 
-    //let date_start = new Date(opt.start_year, opt.start_month - 1);
-
     klawFiles({
         getText: true,
         forFile: (item, next) => {
 
             let d = new Date(item.header.date);
 
-            //if (date.getFullYear() >= 2019 && date.getMonth() + 1 >= 1) {
-            //if (date_post.getTime() >= date_start.getTime()) {
             if (d.getFullYear() == opt.year && d.getMonth() + 1 == opt.month) {
                 console.log(item.header.id, d.getFullYear() + '/' + (d.getMonth()+1), item.header.categories, item.fn, item.wc);
             }
