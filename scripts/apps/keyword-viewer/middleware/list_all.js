4e6587f6455589e452faf751ba0299c45512a046
dustinpfister <dustin.pfister@gmail.com>
2019-04-20 13:30:50 -0400

    list_all

--- scripts/apps/keyword-viewer/middleware/list_all.js
+++ scripts/apps/keyword-viewer/middleware/list_all.js
@@ -1,13 +1,25 @@
 let express = require('express'),
+path = require('path'),
 fs = require('fs'),
-dir_keywords = '../../../keywords';
+dir_keywords = path.resolve('../../../keywords');
 
 // create a router, and use body-parser
 router = module.exports = express.Router();
 
 router.get('*', (req, res, next) => {
 
-    req.filenames = ['lodash-find'];
+    req.filenames = [];
 
-    next();
+    fs.readdir(dir_keywords, (err, files) => {
+
+        files.forEach((fn) => {
+
+            req.filenames.push(path.basename(fn,'.json'));
+
+        });
+
+        next();
+
+    });
+
 });
