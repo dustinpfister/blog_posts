6c89e2ce79ca254f19b4f8b8335a9bfcea00118c
dustinpfister <dustin.pfister@gmail.com>
2019-04-27 13:12:45 -0400

    colors for word count in results

--- scripts/apps/keyword-search/public/js/client.js
+++ scripts/apps/keyword-search/public/js/client.js
@@ -6,15 +6,24 @@ el_out = document.getElementById('search_out');
 el_search_button.addEventListener('click', function (e) {
     http.search(el_search_text.value, function (res) {
         //console.log(res);
-        let data = JSON.parse(res),
+        var data = JSON.parse(res),
         html = '<p>Posts Found: ' + data.match_ct + '</p>';
 
         data.posts.forEach(function (post, i) {
+
+            var color_wc = 'red';
+            if (post.wc >= 500) {
+                color_wc = 'orange';
+            }
+            if (post.wc >= 1000) {
+                color_wc = 'green';
+            }
+
             html += '<p>' + (i + 1) + ') ' + post.fn + '</p>' +
             '<ul>' +
             '<li>keyword full match count: ' + post.fullMatchCount + '<\/li>' +
             '<li>Word Counts: ' + JSON.stringify(post.wordCounts) + '<\/li>' +
-            '<li>Post word count: ' + post.wc + '<\/li>' +
+            '<li style=\"color:' + color_wc + ';\">Post word count: ' + post.wc + '<\/li>' +
             '<li>weight (full match): ' + post.fullMatchWeight + '<\/li>' +
             '<li>weight (word match): ' + post.wordWeight + '<\/li>' +
             '<li>word ratio (kw count / post word count): ' + post.wordRatio + '<\/li>' +
