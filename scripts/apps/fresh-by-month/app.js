dd22f22dadbea4809dd8f832d604173eff7665f6
dustinpfister <dustin.pfister@gmail.com>
2019-05-09 22:12:41 -0400

    days back

--- scripts/apps/fresh-by-month/app.js
+++ scripts/apps/fresh-by-month/app.js
@@ -5,7 +5,7 @@ app = express();
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || process.argv[2] || 8080);
 
-//app.set('dir_cli', path.resolve('../../cli'));
+app.set('days_back', 90);
 
 let dir_cli = path.resolve('../../cli'),
 dir_posts = path.resolve('../../../_posts'),
@@ -14,7 +14,8 @@ klawAll = require(path.join(dir_cli, 'klaw-readall', 'index.js')).klawAll;
 app.get('/', (req, res) => {
 
     let report = {},
-    now = new Date();
+    now = new Date(),
+    days_back = app.get('days_back');
 
     klawAll({
         forPost: (item, next) => {
@@ -22,12 +23,25 @@ app.get('/', (req, res) => {
 
             // the publish date
             let date = new Date(item.header.date),
+            update = new Date(item.header.updated),
             y = date.getFullYear(),
             m = date.getMonth();
 
             let key = y + '-' + (m + 1);
             let month = report[key] = report[key] ? report[key] : {};
+            month.key = key;
             month.wc = month.wc ? month.wc += item.wc : item.wc;
+            month.fresh = month.fresh ? month.fresh : [];
+
+            let days = (now - update) / 1000 / 60 / 60 / 24,
+            fresh = (days_back - days) / days_back;
+            if (fresh < 0) {
+                fresh = 0;
+            }
+            month.fresh.push({
+                title: item.header.title,
+                fresh: fresh
+            });
 
             next();
         },
