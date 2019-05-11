51c9a16fa3d2cbdfc4a625c9218f9cbffa4eddaa
dustinpfister <dustin.pfister@gmail.com>
2019-05-10 23:44:04 -0400

    looking good

--- scripts/apps/level-up/app.js
+++ scripts/apps/level-up/app.js
@@ -37,26 +37,33 @@ app.get('/', [
             let html = '';
             let cap = {
                 level: 100,
-                avgwc: 1200,
+                avgwc: 1000,
                 pc: 1000,
                 wc: 500000
             };
             let level = [
-                        res.reply.wc / cap.wc, // metric 1 (site wide word count)
-                        res.reply.avgwc / cap.avgwc, // metric 2 (avg post word count)
-                        res.reply.pc / cap.pc // metric 3 (post count);
-                    ].map(function (val) {
-                        return val > 1 ? 1 : val;
-                    }).reduce(function (acc, n) {
-                        return acc + n;
-                    }) / 3 * cap.level;
-            html += '<h1>Level: ' + level + '<\/h1>';
+                res.reply.wc / cap.wc, // metric 1 (site wide word count)
+                res.reply.avgwc / cap.avgwc, // metric 2 (avg post word count)
+                res.reply.pc / cap.pc // metric 3 (post count);
+            ].map(function (val) {
+                return val > 1 ? 1 : val;
+            }).reduce(function (acc, n) {
+                return acc + n;
+            }) / 3 * cap.level;
+            html += '<h1>Level: ' + Math.floor(level) + '<\/h1>';
+
             html += '<div style="width:300px;height:30px;background:grey;">' +
             '<div style="width:' + Math.round(level / cap.level * 300) + 'px;height:30px;background:green;"><\/div>' +
+            '<\/div>';
+
+            html += '<div style="width:300px;height:10px;background:#afafff;">' +
+            '<div style="width:' + Math.round((level % Math.floor(level)) * 300) + 'px;height:10px;background:blue;"><\/div>' +
             '<\/div><br>';
+
             html += '<span>Word Count: ' + res.reply.wc + '/' + cap.wc + '<\/span><br>';
             html += '<span>AVG Post Word Count: ' + Math.round(res.reply.avgwc) + '/' + cap.avgwc + '<\/span><br>';
             html += '<span>Post Count: ' + res.reply.pc + '/' + cap.pc + '<\/span><br>';
+            html += '<span>% to next level: ' + (level % Math.floor(level)) + '<\/span><br>';
             res.send(html);
         }
 
