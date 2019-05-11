05cedfb1ea038fc23155d8792adf3b0780e6dee5
dustinpfister <dustin.pfister@gmail.com>
2019-05-10 23:31:44 -0400

    readme

--- scripts/apps/level-up/README.md
+++ scripts/apps/level-up/README.md
@@ -1,1 +1,3 @@
 ## level up
+
+The idea here is that I just have a progress bar of sorts when it comes to continuing to work on writing more content for my site. To some extent it seems like this is like leveling up in a RPG style video game that involves grinding. As long as I just keep it up, eventually I will beat the game sort of speak.
--- scripts/apps/level-up/app.js
+++ scripts/apps/level-up/app.js
@@ -2,10 +2,7 @@ let express = require('express'),
 path = require('path'),
 app = express();
 
-//app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || process.argv[2] || 8080);
-
-//app.set('days_back', 365 * 2);
 
 let dir_cli = path.resolve('../../cli'),
 dir_posts = path.resolve('../../../_posts'),
@@ -15,7 +12,6 @@ app.get('/', [
 
         // get data for all files
         (req, res, next) => {
-
             console.log('klawing posts:');
             res.reply = {
                 wc: 0,
@@ -23,35 +19,29 @@ app.get('/', [
             };
             klawAll({
                 forPost: (item, nextPost) => {
-
                     console.log(item.header.title.substr(0, 30).padEnd(30, '.'));
                     res.reply.wc += item.wc;
                     res.reply.pc += 1;
-
                     nextPost();
                 },
                 onDone: () => {
-
                     console.log('done klawing posts:');
                     res.reply.avgwc = res.reply.wc / res.reply.pc;
-
                     next();
                 }
             });
-
         },
 
         // send report
         (req, res) => {
-
             let html = '';
             let cap = {
                 level: 100,
-                avgwc: 750,
+                avgwc: 1200,
                 pc: 1000,
                 wc: 500000
             };
-            let level = Math.round(m = [
+            let level = [
                         res.reply.wc / cap.wc, // metric 1 (site wide word count)
                         res.reply.avgwc / cap.avgwc, // metric 2 (avg post word count)
                         res.reply.pc / cap.pc // metric 3 (post count);
@@ -59,16 +49,14 @@ app.get('/', [
                         return val > 1 ? 1 : val;
                     }).reduce(function (acc, n) {
                         return acc + n;
-                    }) / 3 * cap.level);
-
+                    }) / 3 * cap.level;
             html += '<h1>Level: ' + level + '<\/h1>';
             html += '<div style="width:300px;height:30px;background:grey;">' +
             '<div style="width:' + Math.round(level / cap.level * 300) + 'px;height:30px;background:green;"><\/div>' +
             '<\/div><br>';
-            html += '<span>Word Count: ' + res.reply.wc + '<\/span><br>';
-            html += '<span>AVG Post Word Count: ' + Math.round(res.reply.avgwc) + '<\/span><br>';
-            html += '<span>Post Count: ' + res.reply.pc + '<\/span><br>';
-
+            html += '<span>Word Count: ' + res.reply.wc + '/' + cap.wc + '<\/span><br>';
+            html += '<span>AVG Post Word Count: ' + Math.round(res.reply.avgwc) + '/' + cap.avgwc + '<\/span><br>';
+            html += '<span>Post Count: ' + res.reply.pc + '/' + cap.pc + '<\/span><br>';
             res.send(html);
         }
 
