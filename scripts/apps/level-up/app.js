f8bcf50f5dc4093088a66547c38d805b38002c16
dustinpfister <dustin.pfister@gmail.com>
2019-05-11 21:51:14 -0400

    getLevel

--- scripts/apps/level-up/app.js
+++ scripts/apps/level-up/app.js
@@ -33,7 +33,7 @@ app.get('/', [
                         res.reply.cat[catName] === undefined ? {}
                      : res.reply.cat[catName];
                     cat.wc = cat.wc === undefined ? item.wc : cat.wc += item.wc;
-                    cat.pc = cat.pc === undefined ? 1 : cat.pc +=1;
+                    cat.pc = cat.pc === undefined ? 1 : cat.pc += 1;
                     cat.name = catName;
 
                     nextPost();
@@ -49,38 +49,43 @@ app.get('/', [
         // send report
         (req, res) => {
             let html = '';
+
+            let getLevel = (cap) => {
+
+                return [
+                    res.reply.wc / cap.wc, // metric 1 (site wide word count)
+                    res.reply.avgwc / cap.avgwc, // metric 2 (avg post word count)
+                    res.reply.pc / cap.pc // metric 3 (post count);
+                ].map(function (val) {
+                    return val > 1 ? 1 : val;
+                }).reduce(function (acc, n) {
+                    return acc + n;
+                }) / 3 * cap.level;
+
+            };
+
             let cap = {
                 level: 100,
                 avgwc: 500,
                 pc: 1000,
                 wc: 500000
             };
-            let level = [
-                res.reply.wc / cap.wc, // metric 1 (site wide word count)
-                res.reply.avgwc / cap.avgwc, // metric 2 (avg post word count)
-                res.reply.pc / cap.pc // metric 3 (post count);
-            ].map(function (val) {
-                return val > 1 ? 1 : val;
-            }).reduce(function (acc, n) {
-                return acc + n;
-            }) / 3 * cap.level;
-            html += '<h1>Level: ' + Math.floor(level) + '<\/h1>';
+            let level = getLevel(cap);
 
+            html += '<h1>Level: ' + Math.floor(level) + '<\/h1>';
             html += '<div style="width:300px;height:30px;background:grey;">' +
             '<div style="width:' + Math.round(level / cap.level * 300) + 'px;height:30px;background:green;"><\/div>' +
             '<\/div>';
-
             html += '<div style="width:300px;height:10px;background:#afafff;">' +
             '<div style="width:' + Math.round((level % Math.floor(level)) * 300) + 'px;height:10px;background:blue;"><\/div>' +
             '<\/div><br>';
-
             html += '<span>Word Count: ' + res.reply.wc + '/' + cap.wc + '<\/span><br>';
             html += '<span>AVG Post Word Count: ' + Math.round(res.reply.avgwc) + '/' + cap.avgwc + '<\/span><br>';
             html += '<span>Post Count: ' + res.reply.pc + '/' + cap.pc + '<\/span><br>';
             html += '<span>% to next level: ' + (level % Math.floor(level)) + '<\/span><br>';
-
             html += '<br><br><table style=\"width:100%;text-align:center;\">';
             html += '<tr><th>Cat name<\/th><th>Word Count<\/th><th>Post Count<\/th><th>AVG Word Count per post<\/th><\/tr>';
+
             let catArr = []
             Object.keys(res.reply.cat).forEach((catName) => {
                 catArr.push(res.reply.cat[catName]);
