07ee4602ebf19a2182bbf498bc0a0de7c7e0e0fd
dustinpfister <dustin.pfister@gmail.com>
2019-06-21 20:37:05 -0400

    public html folder

--- scripts/apps/world-map/app.js
+++ scripts/apps/world-map/app.js
@@ -1,13 +1,32 @@
-let path = require('path'),
+let express = require('express'),
+path = require('path'),
 genMap = require('./lib/genmap.js'),
-dir_posts = path.resolve('../../../_posts');
+dir_posts = path.resolve('../../../_posts'),
+fs = require('fs'),
+app = express();
 
 genMap.fromPosts({
     dir_posts: dir_posts
 })
 
-.then(() => {
+.then((map) => {
 
-    console.log('done');
+    console.log('have a map now');
+
+    fs.mkdir('./public', () => {
+
+        let html = '';
+        map.sections.forEach((section) => {
+            section = JSON.parse(section);
+            html += '<div>' + section.worth + '</div>'
+        });
+        console.log(html);
+        fs.writeFile('./public/index.html', html, () => {
+
+            console.log('done');
+
+        })
+
+    });
 
 })
