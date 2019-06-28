9145e667e5f7bfcbe9607bef13c42c1b45bb1240
dustinpfister <dustin.pfister@gmail.com>
2019-06-28 12:36:08 -0400

    cat count

--- scripts/apps/fresh-by-month/middleware/get_posts.js
+++ scripts/apps/fresh-by-month/middleware/get_posts.js
@@ -8,60 +8,77 @@ module.exports = (opt) => {
 
     klawAll = require(path.join(opt.dir_cli, 'klaw-readall', 'index.js')).klawAll;
 
-    return (req, res, next) => {
+    return [(req, res, next) => {
 
-        let report = res.report = {},
-        now = new Date(),
-        days_back = opt.app.get('days_back');
+            let report = res.report = {},
+            now = new Date(),
+            days_back = opt.app.get('days_back');
 
-        console.log('klawing posts:');
-        klawAll({
-            forPost: (item, nextPost) => {
-                console.log(item.header.title.substr(0, 30).padEnd(30, '.'), item.header.date);
+            console.log('klawing posts:');
+            klawAll({
+                forPost: (item, nextPost) => {
+                    console.log(item.header.title.substr(0, 30).padEnd(30, '.'), item.header.date);
 
-                // the publish date
-                let date = new Date(item.header.date),
-                update = new Date(item.header.updated),
-                y = date.getFullYear(),
-                m = date.getMonth();
+                    // the publish date
+                    let date = new Date(item.header.date),
+                    update = new Date(item.header.updated),
+                    y = date.getFullYear(),
+                    m = date.getMonth();
 
-                let key = y + '-' + (m + 1);
-                let month = report[key] = report[key] ? report[key] : {};
-                month.key = key;
-                month.wc = month.wc ? month.wc += item.wc : item.wc;
-                month.pc = month.pc === undefined ? 0 : month.pc;
-                month.fresh = month.fresh === undefined ? 0 : month.fresh;
-                month.posts = month.posts === undefined ? [] : month.posts;
+                    let key = y + '-' + (m + 1);
+                    let month = report[key] = report[key] ? report[key] : {};
+                    month.key = key;
+                    month.wc = month.wc ? month.wc += item.wc : item.wc;
+                    month.pc = month.pc === undefined ? 0 : month.pc;
+                    month.fresh = month.fresh === undefined ? 0 : month.fresh;
+                    month.posts = month.posts === undefined ? [] : month.posts;
 
-                let days = (now - update) / 1000 / 60 / 60 / 24,
-                fresh = (days_back - days) / days_back;
-                if (fresh < 0) {
-                    fresh = 0;
+                    let days = (now - update) / 1000 / 60 / 60 / 24,
+                    fresh = (days_back - days) / days_back;
+                    if (fresh < 0) {
+                        fresh = 0;
+                    }
+
+                    month.posts.push({
+                        fn: item.fn,
+                        header: item.header,
+                        fresh: fresh,
+                        wc: item.wc
+                    });
+
+                    // month fresh
+                    month.fresh += fresh;
+                    // post count
+                    month.pc += 1;
+
+                    nextPost();
+                },
+                onDone: () => {
+
+                    console.log('done klawing posts:');
+                    console.log('months: ' + Object.keys(res.report).length);
+                    // next middleware
+                    next();
                 }
+            });
 
-                month.posts.push({
-                    fn: item.fn,
-                    header: item.header,
-                    fresh: fresh,
-                    wc: item.wc
+        },
+
+        // built cat object
+        (req, res, next) => {
+            Object.keys(res.report).forEach((key) => {
+                let month = res.report[key];
+                month.cats = {};
+                month.posts.forEach((post) => {
+                    let catName = post.header.categories;
+                    if (typeof catName === 'string') {
+                        month.cats[catName] = month.cats[catName] == undefined ? 1 : month.cats[catName] += 1;
+                    }
                 });
+            });
+            next();
+        }
 
-                // month fresh
-                month.fresh += fresh;
-                // post count
-                month.pc += 1;
-
-                nextPost();
-            },
-            onDone: () => {
-
-                console.log('done klawing posts:');
-                console.log('months: ' + Object.keys(res.report).length);
-                // next middleware
-                next();
-            }
-        });
-
-    };
+    ];
 
 };
