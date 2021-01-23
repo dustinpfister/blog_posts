06472d08b7d2cdce89246b6f78bca23ad324857c
dustinpfister <dustin.pfister@gmail.com>
2021-01-23 12:43:38 -0500

    getting a filename

--- scripts/cli/header/index.js
+++ scripts/cli/header/index.js
@@ -1,12 +1,22 @@
-let path = require('path');
 
-// from a script at blog_posts/scripts/cli/footool/index.js
+// paths
+let path = require('path');
 let dirs = require( path.join(__dirname, '../paths/index.js') ).createDirObject(__dirname);
- 
-console.log('cwd: ', dirs.cwd );                      // current working dir
-console.log('blog_root: ', dirs.blog_root );          // the root of the blog_root project folder
-console.log('this_script: ', dirs.this_script );      // path to this script
-console.log('script_folder: ', dirs.script_folder );  // path to this script
-console.log('app_folder: ', dirs.app_folder );        // path to this script
-console.log('cli_folder: ', dirs.cli_folder );        // path to this script
-console.log('dirs.posts: ', dirs.posts );             // path to this script
+
+// header lib
+let header = require( path.join(dirs.this_script), 'header.js' );
+
+
+let fs = require('fs'),
+fileName = 'canvas-example-animation-basics.md';
+
+let uri_post = path.join(dirs.posts, fileName);
+fs.readFile(uri_post, 'utf8', (e, data) => {
+
+    if(e){
+        console.log(e);
+    }else{
+        console.log(data);
+    }
+
+});
