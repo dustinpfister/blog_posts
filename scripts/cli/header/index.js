3f2893255fc0989e5b1afd09d52eb1e1192f65b5
dustinpfister <dustin.pfister@gmail.com>
2021-01-23 12:57:39 -0500

    json output

--- scripts/cli/header/index.js
+++ scripts/cli/header/index.js
@@ -1,23 +1,22 @@
-
 // paths
 let path = require('path');
 let dirs = require( path.join(__dirname, '../paths/index.js') ).createDirObject(__dirname);
 
 // header lib
-let header = require( path.join(dirs.this_script), 'header.js' );
+let header = require( path.join(dirs.this_script, 'header.js') );
 
-
+// file system, and options
 let fs = require('fs'),
 postName = process.argv[2] || 'canvas-example-animation-basics',
 fileName = postName + '.md';
 
+// read the post, and spit out the header in the standard output
 let uri_post = path.join(dirs.posts, fileName);
-fs.readFile(uri_post, 'utf8', (e, data) => {
-
+fs.readFile(uri_post, 'utf8', (e, text) => {
     if(e){
         console.log(e);
     }else{
-        console.log(data);
+        //var headerObj = header.get(text)
+        console.log(JSON.stringify(header.get(text)));
     }
-
 });
