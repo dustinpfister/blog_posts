1b337d4c10d929755b1b66489507c53cc1d917ae
dustinpfister <dustin.pfister@gmail.com>
2021-05-08 11:54:33 -0400

    getting an internal link count in klaw readall

--- scripts/cli/klaw-readall/index.js
+++ scripts/cli/klaw-readall/index.js
@@ -16,65 +16,59 @@ opt_defaults = {
     onDone: function () {}
 };
 
+let getInteralLinkCount = (linkObjects) => {
+    if (linkObjects.length === 0) {
+        return 0;
+    }
+    if (linkObjects.length === 1) {
+        return Number(!linkObjects[0].external);
+    }
+    return linkObjects.reduce((acc, linkObj) => {
+        acc = typeof acc === 'object' ? Number(!acc.external) : acc;
+        return acc + Number(!linkObj.external);
+    });
+};
+
 let klawAll = (opt) => {
 
     opt = Object.assign({}, opt_defaults, opt || {});
 
     getId().then((nextId) => {
-
         let ct = 0;
-
         // using klaw-basic
         klawPosts({
             forPost: (item, next) => {
-
                 ct += 1;
-
                 fs.readFile(item.path)
-
                 .then((data) => {
-
                     let md = data.toString();
-
                     item.md = header.remove(md);
                     item.header = header.get(md);
                     item.wc = wc.getWC(item.md);
                     item.linkObjects = linkCount(item.md);
-
+                    item.linkInternalCount = getInteralLinkCount(item.linkObjects);
+                    console.log(item.linkInternalCount);
                     item.fn = path.basename(item.path, '.md');
-
                     opt.forPost(item, () => {
-
                         // if ct === nextId then we are done for real
                         if (ct === nextId) {
-
                             opt.onDone();
-
                         } else {
                             next();
                         }
-
                     }, ct);
-
                 })
-
                 .catch((e) => {
-
                     console.log(e.message);
                     next();
                 })
-
             }
-
         })
-
     });
-
 };
 
 // if called from CLI
 if (require.main === module) {
-
     klawAll({
         forPost: (item, next) => {
             console.log(item.header.title, item.wc);
@@ -83,12 +77,8 @@ if (require.main === module) {
         onDone: () => {
             console.log('done');
         }
-
     });
-
 } else {
-
     // else export
     exports.klawAll = klawAll;
-
 }
