e6cf69d5c3ef8dcf1fda66d003fef56ff25f9494
dustinpfister <dustin.pfister@gmail.com>
2021-11-01 09:14:33 -0400

    getting objects for a git log

--- scripts/cli/diff-posts-days/index.js
+++ scripts/cli/diff-posts-days/index.js
@@ -1,35 +1,29 @@
 // paths
 let path = require('path'),
-os = require('os')
-let dirs = require( path.join(__dirname, '../paths/index.js') ).createDirObject(__dirname);
-let spawn = require('child_process').spawn;
+os = require('os'),
+dirs = require(path.join(__dirname, '../paths/index.js')).createDirObject(__dirname),
+spawn = require('child_process').spawn;
 
-let purgeEmpty = (arr) =>{
-	
-	return arr.filter(function(el){
-		return el != '';
-	});
+// purge empty strings from an aray
+let purgeEmpty = (arr) => {
+    return arr.filter(function (el) {
+        return el != '';
+    });
 };
 
 let gitLog = spawn('git', ['log', '-n', '10', '--format=%H/%cD;']);
 let str = '';
-gitLog.stdout.on('data', function(data){
-	str += data.toString();
-	//console.log(data.toString());
-    //console.log( data.toString().split(';;') );
-	
+gitLog.stdout.on('data', function (data) {
+    str += data.toString();
 });
-
-gitLog.on('exit', function(){
-	
-	var arr = purgeEmpty(str.trim().split(';'));
-	
-	arr = arr.map(function(str){
-		
-		return str.split('/');
-		
-	});
-	
-	console.log(arr);
-	
+gitLog.on('exit', function () {
+    var arr = purgeEmpty(str.trim().split(';'));
+    arr = arr.map(function (str) {
+            var a = str.split('/');
+            return {
+                date: new Date(a[1]),
+                hash: a[0]
+            }
+        });
+    console.log(arr);
 });
