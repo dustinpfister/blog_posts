1ce5b2bf0315df1d81a30de9bf2d0c8720144478
dustinpfister <dustin.pfister@gmail.com>
2021-11-01 12:14:12 -0400

    can pass an argument for the number of commits to go back

--- scripts/cli/diff-posts-days/index.js
+++ scripts/cli/diff-posts-days/index.js
@@ -2,7 +2,7 @@ let path = require('path');
 let dObj = require(path.join(__dirname, 'day-objects.js'))
 
 // what calling this script does
-dObj.getHashDateObjects(30)
+dObj.getHashDateObjects(process.argv[2] === undefined ? 30 : process.argv[2])
 .then((hashObjects) => {
     let days = dObj.getDayHashObjects(hashObjects);
     return dObj.getAllChangedFiles(days)
