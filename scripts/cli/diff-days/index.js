841d716d44be4775362c37c4e1dd87516453ab45
dustinpfister <dustin.pfister@gmail.com>
2021-11-02 09:20:12 -0400

    match start only

--- scripts/cli/diff-days/index.js
+++ scripts/cli/diff-days/index.js
@@ -4,7 +4,13 @@ let dObj = require(path.join(__dirname, 'day-objects.js'));
 // only changed files in general
 dObj.onlyFiles(process.argv[2] === undefined ? 30 : process.argv[2])
 .then((days) => {
-    //console.log(days);
+    // filter files for \_posts
+    days = days.map((dayObj) => {
+        dayObj.files = dayObj.files.filter((fileName) => {
+            return !!fileName.match(/^\_posts/)
+        })
+        return dayObj;
+    });
     days.forEach((day) => {
         console.log(day);
     })
