12cb5d36eb29be112bd4c66d8f8d678cf063b746
dustinpfister <dustin.pfister@gmail.com>
2021-11-19 15:00:09 -0500

    getting links method working

--- scripts/apps/post-server/public/js/links.js
+++ scripts/apps/post-server/public/js/links.js
@@ -2,21 +2,15 @@
 var getInteralLinks = function(){
     // get links
     var links = document.querySelectorAll('a');
-
     return [].reduce.call(links, function(acc, a){
         var u = new URL(a.href);
-console.log(u.host);
-console.log(u.pathname);
-console.log('');
         if(u.host === document.location.host){
             acc.push(a);
         }
-
-
         return acc;
     }, []);
 };
 
 var links = getInteralLinks();
-console.log(links);
+console.log(links.map((a)=>{ return a.href;}));
 
