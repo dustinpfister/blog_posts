96f632a67ff357ed575ec97d84299b25450c5962
dustinpfister <dustin.pfister@gmail.com>
2021-11-19 15:12:58 -0500

    highlighting broken internals as red

--- scripts/apps/post-server/public/js/links.js
+++ scripts/apps/post-server/public/js/links.js
@@ -1,3 +1,10 @@
+var httpGetRes = function(url){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp;
+};
+
 // get internal links helper
 var getInteralLinks = function(){
     // get links
@@ -11,10 +18,26 @@ var getInteralLinks = function(){
     }, []);
 };
 
-var links_intern = getInteralLinks();
+var highLightInterals = function(){
+    var links_intern = getInteralLinks();
+    links_intern.forEach(function(a){
+        a.style.background = 'gray';
+    });
+};
 
+highLightInterals();
+
+var links_intern = getInteralLinks();
 links_intern.forEach(function(a){
-   a.style.background = 'gray';
+    var xml = httpGetRes(a.href);
+    if(xml.status === 200){
+        a.style.background = 'lime';
+        console.log(xml.status);
+    }
+    if(xml.status === 404){
+        a.style.background = 'red';
+        console.log(xml.status);
+    }
 });
 
 //console.log(links.map((a)=>{ return a.href;}));
