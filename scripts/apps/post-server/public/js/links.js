2eb31904a4d81d0d28257ef0778f5cb5c7038125
dustinpfister <dustin.pfister@gmail.com>
2021-11-19 14:57:52 -0500

    using url const

--- scripts/apps/post-server/public/js/links.js
+++ scripts/apps/post-server/public/js/links.js
@@ -2,7 +2,19 @@
 var getInteralLinks = function(){
     // get links
     var links = document.querySelectorAll('a');
-    return links;
+
+    return [].reduce.call(links, function(acc, a){
+        var u = new URL(a.href);
+console.log(u.host);
+console.log(u.pathname);
+console.log('');
+        if(u.host === document.location.host){
+            acc.push(a);
+        }
+
+
+        return acc;
+    }, []);
 };
 
 var links = getInteralLinks();
